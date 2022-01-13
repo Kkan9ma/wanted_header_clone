4eb625db5db1a9d7f12dbb4c67fb20ed30906546
Kyeongman Kang <kyengmankang@gmail.com>
2022-01-13 14:33:51 +0900

    스타일 Reset을 위한 전역 컴포넌트 생성

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Main from "./components/Main/Main";
 import NavBar from "./components/NavBar/NavBar";
+import ResetGlobalStyle from "./GlobalStyle";
 
 function App() {
   return (
     <>
+      <ResetGlobalStyle />
       <NavBar />
       <Main />
     </>
