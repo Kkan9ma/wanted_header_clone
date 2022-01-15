bc955ca7850ae72a62fd58a8ca5838c97001a0ee
Kyeongman Kang <kyeongmankang@gmail.com>
2022-01-16 03:45:53 +0900

    반복되는 코드 제거
    - slide에 들어가는 data를 분리하여 반복 코드를 줄임

---
+++ src/datas/SlideDatas.js
@@ -0,0 +1,43 @@
+export const SlickSlideDatas = [
+  {
+    imageURL: 'https://static.wanted.co.kr/images/banners/1473/41f7b36e.jpg',
+    cardTitle: '개발자 되고싶은 분들!?', cardSubTitle: '프론트엔드 무료 교육과정 참여하기'
+  },
+  {
+    imageURL: 'https://static.wanted.co.kr/images/banners/1484/b2853456.jpg',
+    cardTitle: '성장하는 개발자가 되려면?', cardSubTitle: 'OOO 검색하지 말 것!'
+  },
+  {
+
+    imageURL: 'https://static.wanted.co.kr/images/banners/1488/baa54448.jpg',
+    cardTitle: 'UX 디자이너의 커리어 설계', cardSubTitle: '브랜드 가치를 더하는 디자인'
+  },
+  {
+
+    imageURL: 'https://static.wanted.co.kr/images/banners/1487/0d36f0b5.jpg',
+    cardTitle: '포트폴리오를 부탁해!', cardSubTitle: '디자이너의 포폴 살펴보기'
+  },
+  {
+
+    imageURL: 'https://static.wanted.co.kr/images/banners/1468/3df61cbc.jpg',
+    cardTitle: '해, 커리어 EP 02 공개', cardSubTitle: '마지막 관문 2라운드의 승자는 ? '
+  }
+  ,
+  {
+    imageURL: 'https://static.wanted.co.kr/images/banners/1489/312a0c29.jpg',
+    cardTitle: '2022년 달라지는 노동법령', cardSubTitle: '노무관리 쟁점 한 눈에 파악하기'
+
+  },
+  {
+    imageURL: 'https://static.wanted.co.kr/images/banners/1490/0b775035.jpg',
+    cardTitle: '마케팅 주니어를 찾습니다', cardSubTitle: '1월 17일(월) 설명회 신청하기'
+  },
+  {
+    imageURL: 'https://static.wanted.co.kr/images/banners/1460/619f3af7.jpg',
+    cardTitle: '개발자 성장 비결 공개!', cardSubTitle: 'Velog, 글 쓰는 개발자들의 이야기'
+  },
+  {
+    imageURL: 'https://static.wanted.co.kr/images/banners/1486/fba2df30.jpg',
+    cardTitle: '성과를 내는 마케팅', cardSubTitle: '실제 사례를 공개합니다!'
+  }
+]
