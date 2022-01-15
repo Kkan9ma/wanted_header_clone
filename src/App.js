e505b1e0ad674a920206c80577e9c41adbf0b1da
Kyeongman Kang <kyeongmankang@gmail.com>
2022-01-15 18:29:41 +0900

    PaddingDivision 추가

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
+import styled from 'styled-components';
 import Main from "./components/Main/Main";
 import NavBar from "./components/NavBar/NavBar";
 import ResetGlobalStyle from "./GlobalStyle";
+
+const PaddingDivision = styled.div`
+  height: 50px;
+`
 
 function App() {
   return (
     <>
       <ResetGlobalStyle />
       <NavBar />
+      <PaddingDivision />
       <Main />
     </>
   );
